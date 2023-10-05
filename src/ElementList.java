3ff3b9c1688c782cc65e08df144bcf21b62b5e4d
dsarotti <sr.sarotti@gmail.com>
2023-10-05 19:02:02 +0200

    Ejercicio corregido

--- src/ElementList.java
+++ src/ElementList.java
@@ -1,16 +1,24 @@
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.NoSuchElementException;
+/**
+* Lista de elementos de cualquier tipo T.
+* Guarda una lista de elementos y el número de repeticiones que lecorresponde.
+* Guarda cada elemento una sola vez y lleva la cuenta de cuántos sehan guardado en un momento dado.
+*/
 class ElementList<T> {
-    private List<T> elementos;
-    private List<Integer> cantidades;
-
+    private ArrayList<T> elementos;
+    private ArrayList<Integer> cantidades;
     public ElementList() {
         elementos = new ArrayList<T>();
         cantidades = new ArrayList<Integer>();
     }
-
-    public void count(T elemento) {
+    /**
+    * Registra en elemento en la lista.
+    * Si no existe el elemento lo guarda en la lista y se pone elcontador a 1 en la misma posición.
+    * Si ya existe el elemento simplemente incrementa el contador enla posición correspondiente.
+    * @param elemento El elemento a guardar.
+    */
+    public void count(T elemento){
         if (!elementos.contains(elemento)) {
             elementos.add(elemento);
             cantidades.add(1);
@@ -19,28 +27,59 @@ class ElementList<T> {
             cantidades.set(index, cantidades.get(index) + 1);
         }
     }
-
-    public void discount(T elemento){
-        if(elementos.contains(elemento)){
-            int index = elementos.indexOf(elemento);
-            //TODO: terminar.
+    
+    /**
+    * Quita un elemento de la lista
+    * Si existe más de un elemento registrado, decrementa el contador
+    * Si solo existe un elemento, lo elimina y elimina también laentrada del contador.
+    * @param elemento
+    * @throws NoSuchElementException si no existe el elemento
+    */
+    public void discount(T elemento) throws NoSuchElementException{
+        int index = elementos.indexOf(elemento);
+        if(index > -1){
+            if (cantidades.get(index)==1){
+                cantidades.remove(index);
+                elementos.remove(index);
+            }else{
+                cantidades.set(index, cantidades.get(index)-1);
+            }
+        }else{
+            throw new NoSuchElementException("No existe el elemento " +
+            elemento);
         }
     }
-
-    public int contarElemento(T elemento) {
-        if (elementos.contains(elemento)) {
-            int index = elementos.indexOf(elemento);
-            return cantidades.get(index);
-        } else {
-            return 0;
+    /**
+    * Elimina el elemento de la lista sin importar la cantidad quetenga.
+    * Elimina también la entrada correspondiente del contador.
+    * @param elemento
+    * @throws NoSuchElementException si no existe el elemento en lalista.
+    */
+    public void delete (T elemento) throws NoSuchElementException{
+        int index = elementos.indexOf(elemento);
+        if (elementos.size() > -1 ){
+            elementos.remove(index);
+            cantidades.remove(index);
+        }else{
+            throw new NoSuchElementException("No existe el elemento " +
+            elemento);
         }
     }
-
-    public List<T> obtenerElementosDistintos() {
-        return elementos;
-    }
-
-    public List<Integer> obtenerNumeroRepeticiones() {
-        return cantidades;
+    /**
+    * Devuelve la cuenta con el formato "[elem: num, elem: num]".
+    * @return String
+    */
+    @Override
+    public String toString(){
+        String stringFinal;
+        String stringTemporal = "";
+        for (int i = 0;i< elementos.size();i++){
+            stringTemporal = stringTemporal+elementos.get(i)+":"+cantidades.get(i);
+            if (i<elementos.size()-1){
+                stringTemporal = stringTemporal + ", ";
+            }
+        }
+        stringFinal="["+stringTemporal+"]";
+        return stringFinal;
     }
 }
